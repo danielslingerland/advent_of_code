09c2b556d4aa17f694f52f77e72f560306d4a724
Daniel Slingerland <dhslingerland@gmail.com>
2024-12-19 21:11:38 +0100

    day 19 shorter

--- 2024/day19.py
+++ 2024/day19.py
@@ -1,43 +1,30 @@
 from tqdm import tqdm
 
-def load_file():
-    with open('data/day19.txt') as f:
-        lines = [line.rstrip('\n') for line in f]
-    return lines
-
-lines = load_file()
+with open('data/day19.txt') as f:
+    lines = [line.rstrip('\n') for line in f]
 towels = lines[0].split(", ")
 
-
 class Token:
-    def __init__(self, pattern, start, end):
-        self.pattern = pattern
+    def __init__(self, start, end):
         self.start = start
         self.end = end
         self.cache = -1
     
     def count(self, maximaal):
         if self.end == maximaal: return 1
-        if self.cache != -1: return self.cache
-        total = 0
-        for token in tokens[self.end]:
-            total += token.count(maximaal)
-        self.cache = total
-        return total
-    
-    def printToken(self):
-        print("Token{pattern: "+self.pattern+", start: "+str(self.start)+", end: "+str(self.end)+"}")
+        if self.cache == -1:
+            self.cache = 0
+            for token in tokens[self.end]:
+                self.cache += token.count(maximaal)
+        return self.cache
 
 total = 0
 for pattern in tqdm(lines[2:]):
     tokens = [[] for i in pattern]
-    
     for towel in towels:
         for i in range(len(pattern)):
             if(pattern[i:].startswith(towel)):
-                tokens[i].append(Token(towel, i, i+len(towel)))
-
-    
+                tokens[i].append(Token(i, i+len(towel)))    
     for zeros in tokens[0]:
         total += zeros.count(len(pattern))
 print()
